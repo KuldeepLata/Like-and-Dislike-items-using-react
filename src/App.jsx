314e89f96e4a30771cf9b5282ce24f849ce81f3a
KuldeepLata <kuldeeplata2004@gmail.com>
2024-12-04 19:56:16 +0530

    dislike and like

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
+import Display from './Display';
+
+
+function App() {
+  return (
+    <Display />
+  )
+}
+
+export default App;
